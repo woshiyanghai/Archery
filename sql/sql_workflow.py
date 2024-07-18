762ba17a2f52d28f2372c7ce041282c471e2e795
yanta <hai.yang@sayweee.com>
2024-07-18 11:29:54 +0800

    修lint问题

--- sql/sql_workflow.py
+++ sql/sql_workflow.py
@@ -309,13 +309,14 @@ def execute(request):
         return render(request, "error.html", context)
     sys_config = SysConfig()
     if (
-            not request.user.is_superuser
-            and on_query_low_peak_time_ddl(workflow_id) is False
+        not request.user.is_superuser
+        and on_query_low_peak_time_ddl(workflow_id) is False
     ):
         start = sys_config.get("query_low_peak_start", 0)
         end = sys_config.get("query_low_peak_end", 24)
         context = {
-            "errMsg": "管理员设置了业务低峰期时间范围:每天%s:00至%s,你只能在业务低峰时间范围执行DDL工单操作!" % (start, end)
+            "errMsg": "管理员设置了业务低峰期时间范围:每天%s:00至%s,你只能在业务低峰时间范围执行DDL工单操作!"
+            % (start, end)
         }
         return render(request, "error.html", context)
     # 获取审核信息
